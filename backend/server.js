38af7ff214b429d7f821d0f11e25b8e9433290db
Mainak Mukherjee <117885693+mainakkaniam@users.noreply.github.com>
2023-11-04 23:11:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "https://realtime-auction.vercel.app",
+    origin: "https://realtime-auction-19mb.vercel.app",
   })
 );
 
@@ -44,7 +44,7 @@ const http = require("http").Server(app);
 const socketIO = require("socket.io")(http, {
   // setted the cors origin to allow connections to react client
   cors: {
-    origin: "https://realtime-auction.vercel.app",
+    origin: "https://realtime-auction-19mb.vercel.app",
   },
 });
 // Pass to socket to handler to handle socket events from sockets/index.js
