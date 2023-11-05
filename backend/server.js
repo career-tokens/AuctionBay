46f4620743ff72a98710f0cf4a6e898f27b2bfb1
mainak <heellomeelo@gmail.com>
2023-11-05 11:04:16 +0530

    c

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,7 @@ const http = require("http").Server(app);
 const socketIO = require("socket.io")(http, {
   // setted the cors origin to allow connections to react client
   cors: {
-    origin: "https://realtime-auction.vercel.app",
+    origin:process.env.ORIGIN,
   },
 });
 // Pass to socket to handler to handle socket events from sockets/index.js
