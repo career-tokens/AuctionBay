02354c49718b6d1efd1e991852c7949b84706490
mainak <heellomeelo@gmail.com>
2023-12-07 21:27:24 +0530

    small change

--- frontend/src/context/authContext/authContextProvider.js
+++ frontend/src/context/authContext/authContextProvider.js
@@ -18,19 +18,20 @@ function AuthContextProvider({ children }) {
     const authReady = async () => {
       try {
         // Make a GET request to the server to check if the user is authenticated or logged in (at the very start when u open the application)
-        const response = await fetch(`${process.env.REACT_APP_TO_BACKEND_URL}/users/login`, {
-          credentials: 'include',
-        });
+        // const response = await fetch(`${process.env.REACT_APP_TO_BACKEND_URL}/users/login`, {
+        //   credentials: 'include',
+        // });
         
-        const res = await response.json();
-        console.log("res=", res);
+        // const res = await response.json();
+        // console.log("res=", res);
         // If the request was successful, set the authentication state to reflect the authenticated user
 
-        if (res.status === "success" || present)//present is our backup plan
+       // if (res.status === "success" ||
+         if (present)//present is our backup plan
         {
           dispatch({
             type: "SET_AUTH_READY",
-            payload: res?res.data.user:present,
+            payload:present,
           });
           console.log("auth ready");
         }
